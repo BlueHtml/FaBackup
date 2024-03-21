b2f2755bce921ec4419d3a67bb75a22750212f89
fly <luxiaofeng006@qq.com>
2024-03-21 22:09:49 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿const string DIR = "dir", TARGET_PATH = "ROOT";
+int _num = 1;
+await foreach (string line in File.ReadLinesAsync(DIR))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        continue;
+    }
+
+    string path = Path.Combine(TARGET_PATH, Path.GetRelativePath(Path.GetPathRoot(line), line));
+    Directory.CreateDirectory(Path.GetDirectoryName(path));
+    await System.IO.File.WriteAllTextAsync(path, line);
+    Console.WriteLine($"line {_num}: ok");
+    _num++;
+}
